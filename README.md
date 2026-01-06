ed3026beb5fb57ad1c620f4846191600d565e696
Jeffrey Chu <jeffrey.chu1@student.unsw.edu.au>
2026-01-06 21:48:19 +1100

    Added basic build files for project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Chess-Engine
-A project where we create a chess engine using relevant chess evaluation techniques to practice skills and learn
+A project that aims to create a chess engine using relevant chess evaluation techniques to practice skills and learn about algorithmically creating a chess engine
---
+++ build/CMakeFiles/chess_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess_engine.dir/src/main.cpp.obj"
+  "CMakeFiles/chess_engine.dir/src/main.cpp.obj.d"
+  "chess_engine.exe"
+  "chess_engine.exe.manifest"
+  "chess_engine.pdb"
+  "libchess_engine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chess_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
